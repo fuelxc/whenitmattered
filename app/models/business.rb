84e7328a7960a966eca91107e641859c0f7f6ae2
Eric Harrison <eharrison@smashtankapps.com>
2020-03-27 20:13:38 -0700

    just make it ok

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,6 @@
 class Business < ApplicationRecord
+  before_save :geocode
+  
   geocoded_by :address do |obj,results|
     if geo = results.first
       obj.latlon = "POINT(#{geo.longitude} #{geo.latitude})"
